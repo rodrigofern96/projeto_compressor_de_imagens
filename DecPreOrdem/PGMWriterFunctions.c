814bdb231a40849dbdfd5f79c6309b1d3b9b1c47
rodrigofern96 <rodrigo.tavares10@aluno.ifce.edu.br>
2025-12-13 12:01:47 -0300

    PGMWriterFunctions.c

--- DecPreOrdem/PGMWriterFunctions.c
+++ DecPreOrdem/PGMWriterFunctions.c
@@ -1,42 +1,37 @@
 #include "PGMWriterFunctions.h"
 #include <stdlib.h>
 
-void escreverPixels(QTree *bloco, unsigned char *matrizPixels, int larg, int alt, int larguraTotal);
-
-void escreverPGM(QTree *tree, FILE *pgm, int larg, int alt) {
-
-    unsigned char *matrizPixels = calloc(larg * alt, 1);
-    if(!matrizPixels){
-        puts("Memoria Insuficiente");
-        exit(1);
+static void preencher(
+    QTree *n,
+    unsigned char *img,
+    int x, int y,
+    int w, int h,
+    int larguraTotal
+) {
+    if (n->folha) {
+        for (int i = y; i < y + h; i++)
+            for (int j = x; j < x + w; j++)
+                img[i * larguraTotal + j] = n->valor;
+        return;
     }
 
-    escreverPixels(tree, matrizPixels, larg, alt, larg);
+    int hw = w / 2;
+    int hh = h / 2;
 
-    fprintf(pgm, "P5\n%d %d\n255\n", larg, alt);
-    
-    fwrite(matrizPixels, 1, larg * alt, pgm);
-
-    free(matrizPixels);
-
+    preencher(n->q1, img, x,         y,         hw,        hh,        larguraTotal);
+    preencher(n->q2, img, x + hw,    y,         w - hw,    hh,        larguraTotal);
+    preencher(n->q3, img, x,         y + hh,    hw,        h - hh,    larguraTotal);
+    preencher(n->q4, img, x + hw,    y + hh,    w - hw,    h - hh,    larguraTotal);
 }
 
+void escreverPGM(QTree *tree, FILE *f, int larg, int alt) {
 
-void escreverPixels(QTree *bloco, unsigned char *matrizPixels, int larg, int alt, int larguraTotal) {
+    unsigned char *img = calloc(larg * alt, 1);
 
-    if (bloco->folha) {
-        for (int i = 0; i < alt; i++)
-            for (int j = 0; j < larg; j++)
-                matrizPixels[i * larguraTotal + j] = bloco->valor;
-    }else {
+    preencher(tree, img, 0, 0, larg, alt, larg);
 
-        int larg2 = larg / 2;
-        int alt2 = alt / 2;
+    fprintf(f, "P5\n%d %d\n255\n", larg, alt);
+    fwrite(img, 1, larg * alt, f);
 
-        escreverPixels(bloco->q1, matrizPixels,                             larg2,        alt2,       larguraTotal);
-        escreverPixels(bloco->q2, matrizPixels + larg2,                     larg-larg2,   alt2,       larguraTotal);
-        escreverPixels(bloco->q3, matrizPixels + (larguraTotal*alt2),       larg2,        alt-alt2,   larguraTotal);
-        escreverPixels(bloco->q4, matrizPixels + (larguraTotal*alt2)+larg2, larg-larg2,   alt-alt2,   larguraTotal);
-    }
-
+    free(img);
 }
